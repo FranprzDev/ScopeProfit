2b647b27a3fd201494c360a031ff0c3135cecb07
FranPrzDev <franciscoperezdeveloper@gmail.com>
2026-09-19 17:32:28 -0300

    fix(api): expose v2 services to Telegram module

--- apps/api/src/core.module.ts
+++ apps/api/src/core.module.ts
@@ -8,6 +8,7 @@ import { DocumentsService } from './modules/documents/documents.service';
 import { StorageService } from './modules/storage/storage.service';
 import { ChangeRequestsService } from './modules/projects/change-requests.service';
 import { TimeEntriesService } from './modules/projects/time-entries.service';
+import { ProfitabilityService } from './modules/projects/profitability.service';
 
 const providers = [
   PrismaService,
@@ -19,6 +20,7 @@ const providers = [
   StorageService,
   ChangeRequestsService,
   TimeEntriesService,
+  ProfitabilityService,
 ];
 
 @Global()
